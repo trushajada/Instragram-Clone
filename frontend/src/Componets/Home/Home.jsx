f4e5c857de40b4d1d47b4ea024b045421b2b6b9b
Your Name <you@example.com>
2025-01-01 02:59:50 +0530

    add code

--- frontend/src/Componets/Home/Home.jsx
+++ frontend/src/Componets/Home/Home.jsx
@@ -8,24 +8,24 @@ import { FiSmile } from "react-icons/fi";
 const Home = () => {
     return (
         <>
-            <div className="home">
+            <div className="home px-3">
                 {/* card */}
                 <div className="container mx-auto">
-                    <div className="card  w-[800px] border justify-center items-center py-7  mx-auto mt-5">
+                    <div className="card  border justify-center items-center py-7 max-w-lg mx-auto mt-5">
                         <div className="card-header flex w-36 items-center">
                             <img src={homeimg} alt="img" className="w-10 h-10 rounded-full mx-auto"/>
                             <h1 className="font-semibold">Trusha-jada</h1>
                         </div>
                         <br />
                         <div className="card-images">
-                            <img src={post} alt="" className="mx-auto w-screen h-[700px]"/>
+                            <img src={post} alt="" className="mx-auto w-full max-h-[700px]"/>
                         </div>
                         <div className="card-content ">
-                       <span><GrFavorite  className="w-10 text-2xl mt-5 "/></span> 
+                       <span><GrFavorite  className="w-10 text-2xl mt-5"/></span> 
                         <p className=" text-md mt-4 ms-2">1 like</p>
                         <p className=" text-md mt-1 ms-2">Amazing is place</p>
                         </div>
-                        <div className="card-comment flex mt-3 border">
+                        <div className="card-comment flex mt-3 border  space-x-2">
                             <span><FiSmile className="text-xl mt-2 w-10"/></span>
                             <input type="text" placeholder="Add a comment" className=" mx-auto flex w-full"/>
                             <button className="commit  p-1 mx-auto flex w-10 text-blue-400 font-semibold outline-none"><p className="text-center"> Post</p></button>
--- frontend/src/Componets/Profile/Profile.jsx
+++ frontend/src/Componets/Profile/Profile.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import homeimg from "../../assets/images/homeimg.jpg";
 
-const Profile=()=>{
-    return(
+const Profile = () => {
+    return (
         <>
-        
+            <div className="profile px-3">
+                <div className="container mx-auto">
+                    <div className="profile-frame  items-center py-7 max-w-lg mx-auto mt-5 flex border-b-4">
+                        <div className="profile-pic flex ">
+                            <img src={homeimg} alt="img" className="w-32 h-32 rounded-full" />
+                        </div>
+                        <div className="profile-data ">
+                            <h1 className="text-3xl text-center font-semibold">Trusha-jada</h1>
+                            <div className="profile-info flex space-x-3 text-center ms-3 mt-4 justify-center">
+                                <p>40 posts</p>
+                                <p>40 followers</p>
+                                <p>40 following</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="profile-pic w-1/3 grid grid-cols-3 gap-4 items-center justify-center mx-auto mt-5 ">
+                        <img src={homeimg} alt="" />
+                        <img src={homeimg} alt="" />
+                        <img src={homeimg} alt="" />
+                        <img src={homeimg} alt="" />
+                        <img src={homeimg} alt="" />
+                        <img src={homeimg} alt="" />
+
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
