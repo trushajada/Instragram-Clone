f28358677e5b94596884242bcb873badd0d877a4
Your Name <you@example.com>
2025-01-02 04:16:38 +0530

    add code

--- frontend/src/Componets/Createpost/Createpost.jsx
+++ frontend/src/Componets/Createpost/Createpost.jsx
@@ -6,9 +6,21 @@ const Createpost = () => {
     const [body , setbody]=useState("");
     const [image , setImage]=useState("")
 
+    // cloudnary
     const postDetail =()=>{
         console.log(body ,image);
-        
+        const data =new FormData()
+        data.append("file",image)
+        data.append("upload_preset","instra-clone")
+        data.append("cloud_name","instra-cloud")
+        fetch("https://api.cloudinary.com/v1_1/instra-cloud/image/upload",{
+            method:"post",
+            body:data
+        }).then(res=>res.json())
+        .then(data =>console.log(data))
+        .catch(err=>console.log(err)
+        )
+    
     }
 
     var loadFile = function(e) {
@@ -29,7 +41,11 @@ const Createpost = () => {
                         </div>
                         <div className="main-div p-3 border-b-2">
                         <img  id="output" className="w-2/3 mx-auto" />
-                            <input type="file" accept="image/*"  onChange={(e)=>{loadFile(e),setImage(e.target.value[0])}}/>
+                            <input type="file" accept="image/*"  onChange={(e)=>{
+                                loadFile(e);
+                                setImage(e.target.files[0])
+                                
+                                }}/>
                         </div>
                         <div className="detail flex items-center">
                             <div className="card-header p-3">
