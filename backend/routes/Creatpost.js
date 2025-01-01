be686bc0d9270eea77ed4a0a72200a465fe845c1
Your Name <you@example.com>
2025-01-02 00:32:05 +0530

    add code

--- backend/routes/Creatpost.js
+++ backend/routes/Creatpost.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
+const RequireLogin = require('../middleware/RequireLogin');
+const POST = mongoose.model("post")
 
-router.post("/Createpost",(req , res)=>{
-    const {title ,body}=req.body
+router.post("/Createpost",RequireLogin,(req , res)=>{
+    const {title,body}=req.body
     if(!title || !body){
         return res.status(422).json({error:"please add all the fields"})
     }
-    res.json("ok")
+    req.user
+    const post = new POST({
+        title,
+        body,
+        postedBy:req.user
+    })
+    post.save().then((result)=>{
+        return res.json({post:result})
+    }).catch(err=>console.log(err)
+    )
 })
 module.exports=router
