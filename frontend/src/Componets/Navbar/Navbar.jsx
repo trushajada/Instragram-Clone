836433a9cf62b9175cab3f152b736b89dc7e4d04
Your Name <you@example.com>
2025-01-02 23:47:50 +0530

    add code

--- frontend/src/Componets/Navbar/Navbar.jsx
+++ frontend/src/Componets/Navbar/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import Logo from "../../assets/images/Logo.png"
 const Navbar = () => {
+
+    const loginStatus = () => {
+        const token = localStorage.getItem("jwt")
+        if (token) {
+            return [
+                <>
+                    <li>
+                        <a href="Profile" className=" font-semibold">Profile</a>
+                    </li>
+                    <li>
+                        <a href="Createpost" className=" font-semibold">Create post</a>
+                    </li>
+                </>
+            ]
+        } else {
+            return [
+                <>
+                    <li>
+                        <a href="SingUp" className="font-semibold">Sign Up</a>
+                    </li>
+                    <li>
+                        <a href="SingIn" className="font-semibold">Sign In</a>
+                    </li>
+                </>
+            ]
+        }
+    }
     return (
         <>
             <header id="insta" className="bg-gray-100 shadow-lg px-3">
                 <div className="container mx-auto  flex justify-between items-center">
                     <h1 className="w-40 "><img src={Logo} alt="Logo" /></h1>
                     <nav>
-                        <ul className="flex space-x-4"> 
-                            <li>
-                                <a href="SingUp" className="font-semibold">Sign Up</a>
-                            </li>
-                            <li>
-                                <a href="SingIn" className="font-semibold">Sign In</a>
-                            </li>
-                            <li>
-                                <a href="Profile" className=" font-semibold">Profile</a>
-                            </li>
-                            <li>
-                                <a href="Createpost" className=" font-semibold">Create post</a>
-                            </li>
+                        <ul className="flex space-x-4">
+                            {
+                                loginStatus()
+                            }
                         </ul>
                     </nav>
                 </div>
